84db9af6db333bcd70acdeaa4c7591416d373925
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-21 17:35:01 +0200

    Router & Pages Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// page components
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+
+// styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Cooking recipe app</h1>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/recipes/:id" element={<Recipe />}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
