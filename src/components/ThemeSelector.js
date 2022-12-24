f2e2f0d0773440c9287e3530e40346bac583f9a5
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-24 23:31:09 +0200

    Adding dark mode selector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,4 +1,5 @@
 import { useTheme } from "../hooks/useTheme"
+import modeIcon from "../assets/icon_sun.svg"
 
 // styles
 import "./ThemeSelector.css"
@@ -6,11 +7,15 @@ import "./ThemeSelector.css"
 const themeColors = ['#f8ae00', '#0bc50e', '#9825a0']
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light': 'dark');
+  }
   
   return (
     <div className="theme-selector">
-      <div className="theme-buttons">
+        <div className="theme-buttons">
         {themeColors.map(color => (
           <div 
             key={color}
@@ -18,6 +23,14 @@ export default function ThemeSelector() {
             style={{ background: color }}
           />
         ))}
+      </div>
+      <div className="mode-toggle">
+        <img 
+          onClick={toggleMode}
+          src={modeIcon} 
+          alt="dark/light toggle icon"
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+        />
       </div>
     </div>
   )
