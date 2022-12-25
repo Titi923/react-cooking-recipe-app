85b5041116822d83bfed4d7bc6f81d50a850336f
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-25 15:02:26 +0200

    connecting to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiGcdonf8JBezNjIyJKbYiJM2wli08pq0",
+  authDomain: "cookingapp-react-site.firebaseapp.com",
+  projectId: "cookingapp-react-site",
+  storageBucket: "cookingapp-react-site.appspot.com",
+  messagingSenderId: "807451336813",
+  appId: "1:807451336813:web:d6f222f2acff211eea5f64"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
