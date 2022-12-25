22e8c7ca3cfcde8cb0b04892c7ee2dbc0aa8e244
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-25 18:34:25 +0200

    Fetching a single Firestore Document

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,30 +1,47 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-// hooks
-import { useFetch } from '../../hooks/useFetch';
 import { useTheme } from '../../hooks/useTheme';
+import { projectFirestore } from "../../firebase/config"
 
 // styles
 import './Recipe.css';
 
 export default function Recipe() {
   const { id } = useParams();
-  const url = 'http://localhost:3000/recipes/' + id;
-  const { data: recipe, isPending, error } = useFetch(url);
-  const { mode } = useTheme()
+  const { mode } = useTheme();
+
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true)
+    
+    projectFirestore.collection('recipes').doc(id).get().then((doc) => {
+      if(doc.exists) {
+        setIsPending(false);
+        setData(doc.data())
+      } else {
+        setIsPending(false);
+        setError("That data is not valid");
+      }
+    })
+    
+  }, [])
+
 
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipe && (
+      {data && (
         <>
-          <h2 className='page-title'>{recipe.title}</h2>
-          <p>Takes {recipe.cookingTime} to cook</p>
+          <h2 className='page-title'>{data.title}</h2>
+          <p>Takes {data.cookingTime} to cook</p>
           <ul>
-            {recipe.ingredients.map( ing => <li key={ing}>{ing}</li>)}
+            {data.ingredients.map( ing => <li key={ing}>{ing}</li>)}
           </ul>
-          <p className="method">{recipe.method}</p>
+          <p className="method">{data.method}</p>
         </>
       )}
     </div>
