54a3b10283964e2d931b191ebed88a48d731ffd0
Titi923 <petrisor.buciuta92@gmail.com>
2022-12-25 16:55:49 +0200

    fetching data from firestore (collection)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,15 +16,30 @@ export default function Home() {
     setIsPending(true)
 
     projectFirestore.collection('recipes').get().then((snapshot) => {
-      console.log(snapshot);
+      if (snapshot.empty) {
+        setError("No recipes to load")
+        setIsPending(false)
+      } else {
+        // console.log(snapshot);
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        
+        setData(results);
+        setIsPending(false);
+      }
+    }).catch((err) => {
+      setError(err.message)
+      setIsPending(false)
     })
-    
+
   }, [])
 
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
-      {isPending && <p className="loading">Loading...</p>}
+      {isPending && <p className="loading dark">Loading...</p>}
       {data && <RecipeList propData={data} />}
     </div>
   );
